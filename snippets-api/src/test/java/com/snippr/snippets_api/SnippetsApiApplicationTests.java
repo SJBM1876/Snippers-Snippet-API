177fc9f565c9fe7513ba3b69cb30853bd044c2eb
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2025-01-10 08:41:45 +0000

    initial commit

---
+++ snippets-api/src/test/java/com/snippr/snippets_api/SnippetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippr.snippets_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
